b3362d6ca96260d25d804cfbb7c40e237daa018e
Robby Pratl <robbypratl@outlook.com>
2023-03-16 21:36:58 -0500

    initial commit

---
+++ HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Scrfeen</Text>
+    </View>
+  );
+}
+export default HomeScreen;
---
+++ Login.js
@@ -0,0 +1,62 @@
+import { Stack, TextInput, Button, Box } from "@react-native-material/core";
+import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { validatePathConfig } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+function Login({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const validateLogin = async (username, password) => {
+    console.log("Made req");
+    fetch("https://taskcam-backend.onrender.com/sign_in", {
+      method: "POST",
+      headers: {
+        username: username,
+        password: password,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: "robby", password: "Pokemon1234$" }),
+    })
+      .then((r) => r.json())
+      .then((r) => {
+        console.log("Response ", r.token);
+        AsyncStorage.setItem("X-Access-Token", r.token).then(
+          navigation.navigate("HomeScreen")
+        );
+      })
+      .catch((e) => {
+        console.log("Error: ", e);
+      });
+  };
+  return (
+    <View>
+      <Stack spacing={4}>
+        <Text>{message}</Text>
+        <TextInput
+          onChangeText={(e) => {
+            setUsername(e);
+          }}
+          variant="standard"
+          helperText="username"
+        ></TextInput>
+        <TextInput
+          onChangeText={(e) => {
+            setPassword(e);
+          }}
+          variant="standard"
+          helperText="password"
+        ></TextInput>
+        <Box w={64} h={32} m={4} center>
+          <Button
+            title="Logi"
+            onPress={() => validateLogin(username, password)}
+          ></Button>
+        </Box>
+      </Stack>
+    </View>
+  );
+}
+export default Login;
